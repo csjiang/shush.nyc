84e0c7e1c76b0044386871e2c136ef329406864d
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-14 03:39:20 -0400

    add send postcard

--- client/components/SendPostcard/SendPostcardComponent.js
+++ client/components/SendPostcard/SendPostcardComponent.js
@@ -1,13 +1,94 @@
 import React, { Component } from 'react';
 import Page from '../Page/PageComponent';
+import { Textfield, Button } from 'react-mdl';
 
 export default class SendPostcard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Dear neighbor, I am writing to inform you that the amount of noise you have been making recently interferes with my quality of life. For the good of the neighborhood and our beautiful world, would you mind turning it down? Thanks so much.',
+      aptNumber: '',
+      sent: false,
+      error: false,
+      url: '',
+      expected_delivery_date: ''
+    };
+  }
+
+
+  handleChange = field => event => {
+    const value = event.target.value;
+
+    this.setState({
+      [`${field}`]: value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { address } = JSON.parse(localStorage.user_address);
+    console.log('asdhoiasjdoiasjdsa');
+
+    fetch('/api/create-postcard', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          message: this.state.message,
+          address_line1: address.street_number + ' ' + address.route,
+          address_line2: this.state.aptNumber ? this.state.aptNumber : '',
+          address_city: address.sublocality_level_1,
+          address_zip: address.postal_code,
+        })
+      })
+      .then(resp => resp.json())
+      .then(({ url, expected_delivery_date }) => this.setState({
+        url,
+        expected_delivery_date
+      }))
+      .catch(err => this.setState({ error: err }));
+  }
+
   render() {
     return (
-      <Page heading='Send Postcard'>
-        <div style={{ width: '70%', margin: 'auto' }}>
-        </div>
-      </Page>
+      <div>
+      {
+        !this.state.sent
+        ? <Page heading='Customize and send an anonymous postcard to your noisy neighbor.'>
+          {
+            this.state.error
+            ? <div>Oh no, an error occurred! {JSON.stringify(this.state.error)}</div>
+            : null
+          }
+          <div style={{ width: '70%', margin: 'auto' }}>
+            <Textfield
+              onChange={this.handleChange('message')}
+              label='Write a custom message, or use our handy-dandy one!'
+              value={this.state.message}
+              floatingLabel
+              rows={5}
+              style={{width: '80%'}}
+            />
+            <Textfield
+              onChange={this.handleChange('aptNumber')}
+              label='Enter the apartment or unit number, if you know it...'
+              value={this.state.aptNumber}
+              floatingLabel
+              style={{width: '60%', display: 'block'}}
+            />
+            <Button primary raised onClick={this.handleSubmit}>Send!</Button>
+          </div>
+        </Page>
+        : <Page heading='Your postcard was successfully sent!'>
+          <div>
+          <h3>View your postcard <a href={this.state.url}>here.</a></h3>
+          <h3>Your expected delivery date is {this.state.expected_delivery_date}. Thanks for using shush.nyc! If you like our service, please tell your friends.</h3>
+          </div>
+        </Page>
+      }
+    </div>
     );
   }
 }
--- server/createApp.js
+++ server/createApp.js
@@ -7,63 +7,61 @@ import MakeLob from 'lob';
 const Lob = MakeLob(process.env.LOB_API_KEY);
 let db;
 
-export default function createApp(express, port) {
+export default function createApp(express) {
   express.use(bodyParser.urlencoded({ extended: true }));
+  express.use(bodyParser.json());
 
-  /*
   MongoClient.connect(process.env.DB_URL, (err, database) => {
     if (err) return console.log(err);
     db = database;
 
-    express.post('/create-postcard', (req, res, next) => {
-      db.collection('postcards').save(req.body, (err, result) => {
-        //form validation on client-side
-        if (err) return console.log(err);
-        console.log(result, 'saved to database');
+    express.post('/api/create-postcard', (req, res, next) => {
+      const { address_line1, address_line2, address_city, address_zip, message } = req.body;
+      console.log(req.body);
 
-        Lob.postcards.create({
-          to: {
-            name: 'filler',
-            address_line1: 'filler',
-            address_line2: 'filler',
-            address_city: 'filler',
-            address_state: 'filler',
-            address_zip: 'filler',
-          },
-          from: {
-            name: 'A concerned neighbor via shush.nyc',
-            address_city: 'New York',
-            address_state: 'NY',
-            address_zip: 'filler',
-            address_country: 'US'
-          },
-          size: '4x6',
-          front: '<html style="padding: 1in; font-size: 50;">Front HTML for {{name}}</html>',
-          back: '<html style="padding: 1in; font-size: 20;">Back HTML for {{name}}</html>',
-          message: 'trollolol'
-        }, function (err, postcard) {
-          if (err) {
-            console.log(err);
-            return res.send({ error: err });
-          }
-          console.log('Postcard to ' + postcard.to.name + ' sent! View it here: ' + postcard.url, ' Your postcard will be delivered on ' + postcard.expected_delivery_date);
-          res.send(postcard);
-        });
+      Lob.postcards.create({
+        to: {
+          address_state: 'NY',
+          name: 'My Dear Neighbor',
+          address_line1: req.body.address_line1,
+          address_line2: req.body.address_line2,
+          address_city: req.body.address_city,
+          address_zip: req.body.address_zip,
+        },
+        from: {
+          name: 'A concerned neighbor',
+          address_line1: 'https://shush.nyc',
+          address_state: 'NY',
+          address_city: req.body.address_city,
+          address_zip: req.body.address_zip,
+        },
+        size: '4x6',
+        front: '<html style="padding: 1in; font-size: 50;">Test</html>',
+        message: req.body.message
+      }, function (err, postcard) {
+        if (err) {
+          console.log(err);
+          return res.send({ error: err });
+        }
+        console.log('Postcard to ' + postcard.to.name + ' sent! View it here: ' + postcard.url, ' Your postcard will be delivered on ' + postcard.expected_delivery_date);
+
+        return db.collection('postcards')
+          .save(postcard, (err, result) => {
+            //form validation on client-side
+            if (err) return console.log(err);
+            console.log(result, 'saved to database');
+            res.send(postcard);
+          });
       });
     });
+    // express.post('create-address', (req, res, next) => {
+    //   db.collection('addresses').save(req.body, (err, result) => {
+    //     //form validation on client-side
+    //     if (err) return res.send({ error: err });
 
-    express.post('create-address', (req, res, next) => {
-      db.collection('addresses').save(req.body, (err, result) => {
-        //form validation on client-side
-        if (err) return res.send({ error: err });
-
-        console.log('saved address to database');
-        res.send(result);
-      });
-    });
-    */
-    express.listen(port, () => {
-      console.log(`server is listening intently on port ${port}!`);
-    });
-  // });
+    //     console.log('saved address to database');
+    //     res.send(result);
+    //   });
+    // });
+  });
 }
